9ec41dc361302481a5a5f9d8b3622ec59e46f55b
Noah Fernandes <noahdecoco@gmail.com>
2020-03-08 10:19:49 +0100

    takeFirst utility

---
+++ src/redux/sagas/utils/take-first.js
@@ -0,0 +1,11 @@
+import { take, fork, call } from "redux-saga/effects";
+
+export function* takeFirst(pattern, saga, ...args) {
+  const task = yield fork(function*() {
+    while (true) {
+      const action = yield take(pattern);
+      yield call(saga, ...args.concat(action));
+    }
+  });
+  return task;
+}
