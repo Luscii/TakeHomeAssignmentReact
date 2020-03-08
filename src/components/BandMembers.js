f286c9fe11988ca9768ce56fd5403ae22c50646d
Noah Fernandes <noahdecoco@gmail.com>
2020-03-08 08:36:24 +0100

    iteration key and useEffect

--- src/components/BandMembers.js
+++ src/components/BandMembers.js
@@ -1,33 +1,34 @@
-import React from 'react';
-import { useSelector, useDispatch } from 'react-redux'
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
-import { actionCreators as membersActions } from '../redux/actions/membersActions'
+import { actionCreators as membersActions } from "../redux/actions/membersActions";
 
-import Loader from './Loader';
+import Loader from "./Loader";
 
 export default function BandMembers() {
-
-  const loading = useSelector(state => state.app.loading)
-  const members = useSelector(state => state.members)
+  const loading = useSelector(state => state.app.loading);
+  const members = useSelector(state => state.members);
 
   const dispatch = useDispatch();
 
-  if (!loading.isLoading && members.length == 0) {
-    dispatch(membersActions.createRequestMembersList());
-  }
+  useEffect(() => {
+    if (!loading.isLoading && members.length === 0) {
+      dispatch(membersActions.createRequestMembersList());
+    }
+  }, [dispatch, loading.isLoading, members.length]);
 
   return (
     <div>
-    <h2>Band Members</h2>
-    { loading.isLoading && <Loader text={loading.text} /> }
+      <h2>Band Members</h2>
+      {loading.isLoading && <Loader text={loading.text} />}
 
-    { members.length > 0 &&
-      <ul>
-      { members.map((member) =>
-       <li>{member}</li>
-    )}
-      </ul>
-    }
+      {members.length > 0 && (
+        <ul>
+          {members.map(member => (
+            <li key={member}>{member}</li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
